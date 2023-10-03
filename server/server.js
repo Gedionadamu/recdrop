0d85a2592def1e7832a68a00fa58d0596591bb14
Gedion Adamu <gedionadamu@gmail.com>
2023-10-03 12:35:42 -0400

    created and connected to DB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = 3000
 const app = express();
 
-app.listen(PORT, ()=>{
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("connected to mongodb")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+app.listen(PORT, () => {
     console.log(`server listening on  ${PORT}`)
 });
